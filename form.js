48b11cf5e6ea8ddee61b081bcdf3622d6a221d30
Muhammad Haseeb <hasbm125@gmail.com>
2022-10-17 19:17:51 +0500

    auto gen username under dev

--- form.js
+++ form.js
@@ -1,10 +1,13 @@
 
 function is_Vaild(){
     let fn= document.getElementById("fname").value;
-    is_len_vaild(fn, 3, 8)
-   /* alert(fn);*/
+    let ln=  document.getElementById("Lname").value;
+   // let un= document.getElementById("username");
 
-    
+    if(  is_len_vaild(fn, 3, 8) == true){
+        is_len_vaild(ln, 3, 8);
+    }
+    genrrate_username(fn,ln);
 }
 
 function is_len_vaild(inputtext, minlen,maxlen){
@@ -13,13 +16,23 @@ let min=minlen;
 let max= maxlen;
 
 if(field.length<min || field.length >max){
+    if(field.length<min ){  
+        alert("Characters are less than 3");
+        return false;}
+        else{
+            alert("Characters are more than 8");
+            return false;
+        }
 
-    alert("Characters are more than 8");
-    return false;
 }
 else{
     return true;
 }
+}
+
+function genrrate_username(fn,ln){
+
+
 
 
 }
