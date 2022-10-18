655f5c14888f936ec3c27e396fb7c1710d84e133
Muhammad Haseeb <hasbm125@gmail.com>
2022-10-18 14:40:37 +0500

    added new functions

--- form.js
+++ form.js
@@ -1,38 +1,66 @@
 
-function is_Vaild(){
-    let fn= document.getElementById("fname").value;
-    let ln=  document.getElementById("Lname").value;
-   // let un= document.getElementById("username");
+function is_Vaild() {
+    let fn = document.getElementById("fname").value;
+    let ln = document.getElementById("Lname").value;
+    // let un= document.getElementById("username");
 
-    if(  is_len_vaild(fn, 3, 8) == true){
+    if (is_len_vaild(fn, 3, 8) == true) {
         is_len_vaild(ln, 3, 8);
     }
-    genrrate_username(fn,ln);
+   // check_age();
+   check_age();
+   check_mail();
 }
 
-function is_len_vaild(inputtext, minlen,maxlen){
-let field=inputtext;
-let min=minlen;
-let max= maxlen;
+function is_len_vaild(inputtext, minlen, maxlen) {
+    let field = inputtext;
+    let min = minlen;
+    let max = maxlen;
 
-if(field.length<min || field.length >max){
-    if(field.length<min ){  
-        alert("Characters are less than 3");
-        return false;}
-        else{
+    if (field.length < min || field.length > max) {
+        if (field.length < min) {
+            alert("Characters are less than 3");
+            return false;
+        }
+        else {
             alert("Characters are more than 8");
             return false;
         }
-
+    }
+    else {
+        return true;
+    }
 }
-else{
+
+function genrate_username() {
+
+    let fn = document.getElementById("fname").value;
+    let ln = document.getElementById("Lname").value;
+
+    let fl=fn.slice(0,3);
+    let ll=ln.slice(-3);
+
+    let X=fl+ll;
+  document.getElementById("username").value=X;
+    
+}
+
+function check_age(){
+    let A=document.getElementById("age").value;
+    if(A>50){
+        alert("Age is greater than 50");
+         return false;
+    }
     return true;
 }
-}
-
-function genrrate_username(fn,ln){
-
-
-
+function check_mail()
+{
+    let mail=document.getElementById("email").value;
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ ;
+    if(mail.match(mailformat)){
+    }
+    else{
+        alert("Email don't match the format");
+    }
 
 }
